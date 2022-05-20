65b02bf1189b28d1975aca5345accd90a1cc98cb
yjoonjang <yjoonjang34@gmail.com>
2022-05-20 20:41:34 +0900

    feat : 로그인 화면 구성 테스트

--- pages/Login.js
+++ pages/Login.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import kakao_button_image from '../src/images/kakao_login_medium_narrow.png';
+import React from "react";
+import styled from "styled-components";
+// import kakao_button_image from '../src/images/kakao_login_medium_narrow.png'; --Todo
 
 export const kakao_button = styled.img`
   width: 60px;
@@ -9,7 +9,7 @@ export const kakao_button = styled.img`
 export default function Login() {
   return (
     <>
-      <div>로그인 화면입니다.</div>
+      <div>로그인 화면입니다..</div>
       <div>
         <a href={KAKAO_AUTH_URL}></a>
       </div>
@@ -18,6 +18,6 @@ export default function Login() {
 }
 
 // KAKAO_AUTH_URL은 다른 페이지에서 관리
-const CLIENT_ID = 'REST API 부분을 넣어준다.';
-const REDIRECT_URI = '설정한 리다이렉트 URL을 넣어준다';
+const CLIENT_ID = "REST API 부분을 넣어준다.";
+const REDIRECT_URI = "설정한 리다이렉트 URL을 넣어준다";
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
