ffbc0eda4dac5ce50d2b6be1a6c584c811804e58
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-06-10 02:00:29 +0900

    fix: nextjs recoil 아톰키 중복 에러

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// /** @type {import('next').NextConfig} */
+const intercept = require("intercept-stdout"); // TODO: recoil ssr 관련 에러있음 임시코드라 업데이트시 확인필요
+
+// safely ignore recoil warning messages in dev (triggered by HMR)
+function interceptStdout(text) {
+  if (text.includes("Duplicate atom key")) {
+    return "";
+  }
+  return text;
 }
 
-module.exports = nextConfig
+if (process.env.NODE_ENV === "development") {
+  intercept(interceptStdout);
+}
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
