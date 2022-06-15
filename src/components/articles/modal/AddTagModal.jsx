ea5486acc5a38f753563b7d6d885d26757fe4c76
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-06-15 23:00:12 +0900

    feat: addtagmodal 스타일작업

--- src/components/articles/modal/AddTagModal.jsx
+++ src/components/articles/modal/AddTagModal.jsx
@@ -3,16 +3,36 @@ import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { tagState } from "../../../state/addProjectState";
 import { modalListState } from "../../../state/modalState";
+import { BLACK, GRAY500, GRAY700, ORANGE } from "../../../styles/theme";
+import CustomBtn from "../../elements/CustomBtn";
+import CustomTextInput from "../../elements/CustomTextInput";
 
-export const Modal = styled("div")`
-  width: 500px;
-  height: 300px;
-  background-color: white;
+// TODO: 모달에 자주 쓰이는 요소 스타일드 컴포넌트 파일화, x 버튼 추가, 모달 배경 변경
+const Modal = styled.div`
+  width: 640px;
+  height: 288px;
+  background: #ffffff;
+  border-radius: 20px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: center;
-  padding: 60px 0;
+  padding: 56px 0 28px 0;
+`;
+
+const Title = styled.div`
+  color: ${BLACK};
+  margin-bottom: 8px;
+`;
+
+const SubTitle = styled.div`
+  color: ${GRAY500};
+  margin-bottom: 28px;
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  gap: 12px;
+  margin-top: 1.5rem;
 `;
 
 const AddTagModal = () => {
@@ -40,12 +60,28 @@ const AddTagModal = () => {
 
   return (
     <Modal>
-      addtagmodal
-      <input type="text" onChange={onChange} />
-      <div>
-        <button onClick={onClickCloseModal}>취소</button>
-        <button onClick={onClickAddTag}>확인</button>
-      </div>
+      <Title className="ko-headline-6">프로젝트 Tool 추가등록</Title>
+      <SubTitle>프로젝트 Tool</SubTitle>
+      <CustomTextInput
+        id="tag-name"
+        placeholder="텍스트"
+        value={tagName}
+        onChange={onChange}
+        width="420px"
+        height="48px"
+      />
+      <BtnWrapper>
+        <CustomBtn
+          text="취소"
+          onClick={onClickCloseModal}
+          width="99px"
+          height="48px"
+          borderRadius="5px"
+          border="1px solid black"
+          color={GRAY700}
+        />
+        <CustomBtn text="확인" onClick={onClickAddTag} width="99px" height="48px" borderRadius="5px" bgColor={ORANGE} />
+      </BtnWrapper>
     </Modal>
   );
 };
--- src/components/settingPage/ProjectSummary.jsx
+++ src/components/settingPage/ProjectSummary.jsx
@@ -1,11 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+import { GRAY300, GRAY500, GRAY900 } from "../../styles/theme";
 import { Box, Title } from "./AddProjectPage.style";
 
+const TextAreaWrapper = styled.div.attrs((props) => ({
+  hasValue: props.hasValue,
+  isError: props.isError,
+}))`
+  position: relative;
+  height: 64px;
+  border: 1px solid ${GRAY300};
+  border-radius: 4px;
+  display: flex;
+
+  & > textarea {
+    flex: 1;
+    box-sizing: border-box;
+    margin: 8px 16px 8px 16px;
+    resize: none;
+    border: none;
+    outline: none;
+    color: ${GRAY900};
+
+    &::placeholder {
+      color: ${GRAY500};
+    }
+  }
+
+  &:hover {
+    border: 1px solid ${GRAY500};
+  }
+
+  ${({ hasValue }) =>
+    hasValue &&
+    css`
+      border: 1px solid ${GRAY900};
+      &:hover {
+        border: 1px solid ${GRAY900};
+      }
+    `};
+
+  ${({ isError }) =>
+    isError &&
+    css`
+      border: 1px solid rgba(255, 90, 0, 0.7);
+      &:hover {
+        border: 1px solid rgba(255, 90, 0, 0.7);
+      }
+    `};
+`;
+
+const Description = styled.p`
+  margin: 0;
+  position: absolute;
+  bottom: -24px;
+  left: 0;
+  color: rgba(255, 90, 0, 0.7);
+`;
+
 const ProjectSummary = () => {
+  const [summaryText, setSummaryText] = useState("");
+  const isError = summaryText.length > 99; // TODO: usememo
+
+  const onChange = (e) => {
+    setSummaryText(e.target.value);
+  };
+
   return (
     <Box>
       <Title className="ko-headline-6">프로젝트 한줄소개</Title>
-      <textarea name="" id="" rows={5} cols={4}></textarea>
+      <TextAreaWrapper hasValue={summaryText.length > 0} isError={isError}>
+        <textarea
+          className="ko-body-2"
+          placeholder="서비스 기획자를 대상으로한 회고록 서비스 입니다.&#13;&#10;ex) 쉬운 회고록 템플릿 제시하여 회고 습관 형성"
+          value={summaryText}
+          onChange={onChange}
+        />
+        {isError && <Description className="ko-body-2">100자 이상은 입력할 수 없어요</Description>}
+      </TextAreaWrapper>
     </Box>
   );
 };
