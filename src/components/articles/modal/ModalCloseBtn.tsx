c016751a9b5616a11ba114576d9f34b7cb05f9cc
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-06-17 00:53:13 +0900

    feat: 모달 closeicon 추가

---
+++ src/components/articles/modal/ModalCloseBtn.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CloseIcon from '../../elements/CloseIcon'
+
+const ModalCloseBtn = () => {
+  return (
+    <span style={{ position: 'absolute', top: 0, right: 0 }}>
+      <CloseIcon color="black" width={16.33} height={16.33} />
+    </span>
+  )
+}
+
+export default ModalCloseBtn
