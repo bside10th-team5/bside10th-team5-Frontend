a2920ac6faef5bca1a8e7af48a327e3e6540d72d
seung-ik <5seungik@traum.biz>
2022-06-27 19:38:30 +0900

    feat: fullcalendar 라이브러리, nextjs css 호환 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 // /** @type {import('next').NextConfig} */
+const withTM = require("next-transpile-modules")([
+  // fullcalendar 라이브러리 모듈에서 css 불러올때 필요한거
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+]);
+
 const intercept = require("intercept-stdout"); // TODO: recoil ssr 관련 에러있음 임시코드라 업데이트시 확인필요
 
 // safely ignore recoil warning messages in dev (triggered by HMR)
@@ -20,4 +30,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withTM(nextConfig);
