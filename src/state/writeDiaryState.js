df920ce159fe724f5d3fef8aaeea918186f0b31f
seung-ik <5seungik@traum.biz>
2022-06-30 19:53:54 +0900

    feat: fullcalendar 달력 기능추가

--- src/state/writeDiaryState.js
+++ src/state/writeDiaryState.js
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
 
+export const selectedDateState = atom({
+  key: "writeDiary/selectedDate",
+  default: new Date(),
+});
+
 export const templateListState = atom({
   key: "writeDiary/templateList",
   default: [],
