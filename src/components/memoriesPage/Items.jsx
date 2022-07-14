d0815f0621a29be5193c305ad24d1e8e6c664027
seung-ik <5seungik@traum.biz>
2022-07-14 18:55:27 +0900

    feat: lt page 데이터 패치 테스트

--- src/components/memoriesPage/Items.jsx
+++ src/components/memoriesPage/Items.jsx
@@ -1,106 +1,128 @@
+import axios from "axios";
 import { useRouter } from "next/router";
 import React from "react";
+import { useQuery } from "react-query";
 import { useSetRecoilState } from "recoil";
+import { useProjectList } from "../../state/memoriesState";
 import { modalListState } from "../../state/modalState";
 import { WHITE } from "../../styles/theme";
 import CloseIcon from "../elements/CloseIcon";
 import { ItemWrapper, ItemBox, ItemTitle, ItemType, ItemDate, CloseIconWrapper } from "./MemoriesPage.style";
 
 const Items = () => {
-  const mockItemList = [
-    {
-      id: 1,
-      src: "img/thumbnail/side_project.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
+  const { fetch } = useProjectList();
+  const { data: projectList } = useQuery(["projectList"], fetch, {
+    select: (data) => {
+      const { content } = data.data;
+      const normalized = content.map((el) => {
+        return {
+          id: el.id,
+          src: `img/thumbnail/${el.thumbnailId}.png`,
+          title: el.title,
+          type: el.projectType,
+          startDate: el.fromDate.slice(2),
+          endDate: el.toDate,
+        };
+      });
+      return normalized;
     },
-    {
-      id: 2,
-      src: "img/thumbnail/project.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "자유형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 3,
-      src: "img/thumbnail/growing_up.png",
-      title:
-        "길게길게 프로젝트 회고록 길게길게 프로젝트 회고록 길게길게 프로젝트 회고록 길게길게 프로젝트 회고록 길게길게 프로젝트 회고록",
-      type: "자유형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 4,
-      src: "img/thumbnail/learned.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 5,
-      src: "img/thumbnail/worklog.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 6,
-      src: "img/thumbnail/worklog.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 7,
-      src: "img/thumbnail/side_project.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 8,
-      src: "img/thumbnail/project.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 9,
-      src: "img/thumbnail/growing_up.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "자유형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 10,
-      src: "img/thumbnail/learned.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 11,
-      src: "img/thumbnail/worklog.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 12,
-      src: "img/thumbnail/worklog.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "프로젝트형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-    {
-      id: 13,
-      src: "img/thumbnail/side_project.png",
-      title: "비사이드 프로젝트 회고록",
-      type: "자유형",
-      date: "22.22.22 ~ 22.22.22",
-    },
-  ];
+  });
+  console.log(projectList);
+
+  // const mockItemList = [
+  //   {
+  //     id: 1,
+  //     src: "img/thumbnail/side_project.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 2,
+  //     src: "img/thumbnail/project.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "자유형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 3,
+  //     src: "img/thumbnail/growing_up.png",
+  //     title:
+  //       "길게길게 프로젝트 회고록 길게길게 프로젝트 회고록 길게길게 프로젝트 회고록 길게길게 프로젝트 회고록 길게길게 프로젝트 회고록",
+  //     type: "자유형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 4,
+  //     src: "img/thumbnail/learned.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 5,
+  //     src: "img/thumbnail/worklog.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 6,
+  //     src: "img/thumbnail/worklog.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 7,
+  //     src: "img/thumbnail/side_project.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 8,
+  //     src: "img/thumbnail/project.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 9,
+  //     src: "img/thumbnail/growing_up.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "자유형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 10,
+  //     src: "img/thumbnail/learned.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 11,
+  //     src: "img/thumbnail/worklog.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 12,
+  //     src: "img/thumbnail/worklog.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "프로젝트형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  //   {
+  //     id: 13,
+  //     src: "img/thumbnail/side_project.png",
+  //     title: "비사이드 프로젝트 회고록",
+  //     type: "자유형",
+  //     date: "22.22.22 ~ 22.22.22",
+  //   },
+  // ];
   const router = useRouter();
   const setModalList = useSetRecoilState(modalListState);
 
@@ -122,7 +144,7 @@ const Items = () => {
       <ItemBox onClick={onClickAddProject}>
         <img src="img/thumbnail/new.png" />
       </ItemBox>
-      {mockItemList.map((el) => (
+      {projectList?.map((el) => (
         <ItemBox key={el.id} onClick={onClickProject}>
           <CloseIconWrapper type="button" onClick={openDeleteProjectModal}>
             <CloseIcon color={WHITE} width={16.33} height={16.33} />
@@ -131,7 +153,7 @@ const Items = () => {
           <ItemTitle className="headline-6">{el.title}</ItemTitle>
           <div className="item-ref">
             <ItemType className="body-2">{el.type}</ItemType>
-            <ItemDate className="body-2">{el.date}</ItemDate>
+            <ItemDate className="body-2">{`${el.startDate} ~ ${el.endDate}`}</ItemDate>
           </div>
         </ItemBox>
       ))}
