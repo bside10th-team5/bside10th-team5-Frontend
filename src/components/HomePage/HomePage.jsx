c804eb806dd74c2c63a5c350bc375791e02448a5
yjoonjang <yjoonjang34@gmail.com>
2022-07-16 00:18:44 +0900

    fix: 이미지 파일 경로 수정 및 로그인 유무 판별 기능 추가

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,20 @@
 import Gnb from "../articles/Gnb";
+import { useRouter } from "next/router";
 import { WideWrapper } from "../elements/Wrapper.style";
 import { MainTitleWrapper, Section, ItemWrapper, ContentWrapper, Wrapper, StartButton, BigText, SmallText, BgItemWrapper, BgTitleWrapper, EmogiImage, PageImage, SliderArrowContainer, Dot, SelectedDot } from "./HomePage.styled";
 import { useState } from "react";
+import { isLoggedInState } from "../../state";
+import { useRecoilValue } from "recoil";
 
 const HomePage = () => {
+    const router = useRouter()
     const [scrollHeight, setScrollHeight] = useState(0)
+    const isLoggedIn = useRecoilValue(isLoggedInState)
+
+
+    const onClickLoginButton = () => {
+        router.push('/login')
+    }
 
     const SliderArrow = () => {
         const onClickUpArrow = () => {
@@ -19,12 +29,12 @@ const HomePage = () => {
 
         return (
             <SliderArrowContainer>
-                <button onClick={onClickUpArrow}><img src="/icon/slider/arrow/up.svg" /></button>
+                <button onClick={onClickUpArrow}><img src="/img/home/icon/slider/arrow/up.svg" /></button>
                 {(scrollHeight >= 0 && scrollHeight < 1080) ? <SelectedDot />: <Dot /> }
                 {(scrollHeight >= 1080 && scrollHeight < 1080 * 2) ? <SelectedDot />: <Dot /> }
                 {(scrollHeight >= 1080 * 2 && scrollHeight < 1080 * 3) ? <SelectedDot />: <Dot /> }
                 {(scrollHeight >= 1080 * 3 && scrollHeight < 1080 * 4) ? <SelectedDot />: <Dot /> }
-                <button onClick={onClickDownArrow}><img src="/icon/slider/arrow/down.svg" /></button>
+                <button onClick={onClickDownArrow}><img src="/img/home/icon/slider/arrow/down.svg" /></button>
             </SliderArrowContainer>
         )
     }
@@ -38,7 +48,7 @@ const HomePage = () => {
                     <ContentWrapper>
                             <Wrapper>
                                 <div>
-                                    <img src="/images/home_img.svg" style={{width: "37vw"}}/>
+                                    <img src="/img/home/main_img.svg" style={{width: "37vw"}}/>
                                 </div>
                                 <div>
                                     <MainTitleWrapper>
@@ -46,7 +56,7 @@ const HomePage = () => {
                                             오늘 무슨 일이 일어났나요?
                                         </span>
                                         <BigText className="subtitle-1 ">서비스 기획자를 위한<BigText className="subtitle-2">회고록 서비스</BigText></BigText>
-                                        <StartButton className="subtitle-2">시작하기</StartButton>
+                                        {isLoggedIn ? <div></div> : <StartButton className="subtitle-2" onClick={onClickLoginButton}>시작하기</StartButton>}
                                     </MainTitleWrapper>
                                 </div>
                             </Wrapper>
@@ -55,16 +65,16 @@ const HomePage = () => {
                 </ItemWrapper>
                 <BgItemWrapper>
                     <BgTitleWrapper>
-                        <EmogiImage src="/emoji/add_folder.svg" />
+                        <EmogiImage src="/img/home/emoji/add_folder.svg" />
                         <BigText className="subtitle-2" style={{color: '#0B70FE'}}>하루 5분 투자로<BigText className="subtitle-2" style={{color:'black' }}>미리 준비하는 포트폴리오</BigText></BigText>
                         <SmallText className="subtitle-1">중요 정보와 자료를 미리미리 기록!</SmallText>
                     </BgTitleWrapper>
-                    <PageImage src="/images/img_LT.svg" />
+                    <PageImage src="/img/home/img_LT.svg" />
                     <SliderArrow />
                 </BgItemWrapper>
                 <BgItemWrapper>
                     <BgTitleWrapper>
-                        <EmogiImage src="/emoji/bulb.svg" />
+                        <EmogiImage src="/img/home/emoji/bulb.svg" />
                         <BigText className="subtitle-2" style={{color: '#03C348'}}>
                             업무 상황에 맞는
                             <BigText className="subtitle-2" style={{color:'black' }}>
@@ -73,16 +83,16 @@ const HomePage = () => {
                         </BigText>
                         <SmallText className="subtitle-1">설계서형, IA형 등 다양한 템플릿</SmallText>
                     </BgTitleWrapper>
-                    <PageImage src="/images/img_WR.svg" />
+                    <PageImage src="/img/home/img_WR.svg" />
                     <SliderArrow />
                 </BgItemWrapper>
                 <BgItemWrapper>
                     <BgTitleWrapper>
-                        <EmogiImage src="/emoji/worker.svg" />
+                        <EmogiImage src="/img/home/emoji/worker.svg" />
                         <BigText className="subtitle-2">디자이너, 개발자 등<BigText className="subtitle-2" style={{color:'#FF9F00' }}>다양한 협업 히스토리 <span className="subtitle-2" style={{fontSize:'48px', color: "black" }}>기록</span></BigText></BigText>
                         <SmallText className="subtitle-1">왓헤픈과 함께 커뮤니케이션 스킬도 UP!</SmallText>
                     </BgTitleWrapper>
-                    <PageImage src="/images/img_WR.2.svg" />
+                    <PageImage src="/img/home/img_WR.2.svg" />
                     <SliderArrow />
                 </BgItemWrapper>
             </Section>
