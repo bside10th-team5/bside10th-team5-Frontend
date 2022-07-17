c215a0ced25fea7582bce35b9c89b4231649fc39
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-07-18 01:04:27 +0900

    landing_page 스타일 수정

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -23,15 +23,39 @@ import { useState } from "react";
 import { isLoggedInState } from "../../state";
 import { useRecoilValue } from "recoil";
 import Footer from "../articles/Footer";
+import React, { useRef } from "react";
 
 const HomePage = () => {
   const router = useRouter();
+  const ref = useRef(null);
   const [scrollHeight, setScrollHeight] = useState(0);
   const isLoggedIn = useRecoilValue(isLoggedInState);
+  const [scrollDetect, setScrollDetect] = useState("");
+  const [step, setStep] = useState(0);
 
   const onClickLoginButton = () => {
     router.push("/login");
   };
+
+  React.useEffect(() => {
+    let lastScrollY = 0;
+    window.addEventListener("scroll", (e) => {
+      //TODO : 리턴
+      const scrollY = window.scrollY;
+      const direction = scrollY > lastScrollY + 10 ? "down" : "up";
+      lastScrollY = scrollY;
+      console.log(direction);
+      setScrollDetect(direction);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    // console.log(step);
+    // if (scrollDetect === "down" && step === 0) {
+    //   setStep(1);
+    //   window.scrollTo(0, window.innerHeight - 80);
+    // }
+  }, [scrollDetect]);
 
   const SliderArrow = () => {
     const onClickUpArrow = () => {
@@ -40,21 +64,22 @@ const HomePage = () => {
     };
 
     const onClickDownArrow = () => {
-      window.scrollBy(0, 1080);
-      setScrollHeight(window.scrollY);
+      console.log(window.innerHeight);
+      window.scrollTo(0, window.innerHeight - 80);
+      // setScrollHeight(window.scrollY);
     };
 
     return (
       <SliderArrowContainer>
         <button onClick={onClickUpArrow}>
-          <img src="/img/home/icon/slider/arrow/up.svg" />
+          <img src="/img/home/icon/slider/arrow/up.svg" style={{ width: "3.3vw", height: "3.3vw" }} />
         </button>
         {scrollHeight >= 0 && scrollHeight < 1080 ? <SelectedDot /> : <Dot />}
         {scrollHeight >= 1080 && scrollHeight < 1080 * 2 ? <SelectedDot /> : <Dot />}
         {scrollHeight >= 1080 * 2 && scrollHeight < 1080 * 3 ? <SelectedDot /> : <Dot />}
         {scrollHeight >= 1080 * 3 && scrollHeight < 1080 * 4 ? <SelectedDot /> : <Dot />}
         <button onClick={onClickDownArrow}>
-          <img src="/img/home/icon/slider/arrow/down.svg" />
+          <img src="/img/home/icon/slider/arrow/down.svg" style={{ width: "3.3vw", height: "3.3vw" }} />
         </button>
       </SliderArrowContainer>
     );
@@ -62,81 +87,120 @@ const HomePage = () => {
 
   // const
   return (
-    <WideWrapper>
+    <WideWrapper ref={ref}>
       <Gnb isVisible />
       <Section>
         <ItemWrapper>
-          <ContentWrapper>
-            <BackgroundImageContainer />
-            <Wrapper>
-              <div>
-                <img src="/img/home/main_img.svg" style={{ width: "37vw" }} />
-              </div>
-              <div>
-                <MainTitleWrapper>
-                  <span className="main-title-1">오늘 무슨 일이 일어났나요?</span>
-                  <BigText className="subtitle-1 ">
-                    서비스 기획자를 위한<BigText className="subtitle-2">회고록 서비스</BigText>
-                  </BigText>
-                  {isLoggedIn ? (
-                    <div></div>
-                  ) : (
-                    <StartButton type="button" className="subtitle-2" onClick={onClickLoginButton}>
-                      시작하기
-                    </StartButton>
-                  )}
-                </MainTitleWrapper>
-              </div>
-            </Wrapper>
-          </ContentWrapper>
+          {/* <BackgroundImageContainer /> */}
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "5.8%",
+            }}
+          >
+            <div>
+              <img src="/img/home/main_img.svg" style={{ width: "37vw" }} />
+            </div>
+            <div>
+              <MainTitleWrapper>
+                <span className="main-title-1">오늘 무슨 일이 일어났나요?</span>
+                <BigText className="subtitle-1 ">
+                  서비스 기획자를 위한<BigText className="subtitle-2">회고록 서비스</BigText>
+                </BigText>
+                {isLoggedIn ? (
+                  <div></div>
+                ) : (
+                  <StartButton type="button" className="subtitle-2" onClick={onClickLoginButton}>
+                    시작하기
+                  </StartButton>
+                )}
+              </MainTitleWrapper>
+            </div>
+          </div>
           <SliderArrow />
         </ItemWrapper>
         <BgItemWrapper>
-          <BgTitleWrapper>
-            <EmogiImage src="/img/home/emoji/add_folder.svg" />
-            <BigText className="subtitle-2" style={{ color: "#0B70FE" }}>
-              하루 5분 투자로
-              <BigText className="subtitle-2" style={{ color: "black" }}>
-                미리 준비하는 포트폴리오
+          <div
+            style={{
+              display: "flex",
+
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "5.4%",
+            }}
+          >
+            <BgTitleWrapper>
+              <EmogiImage src="/img/home/emoji/add_folder.svg" />
+              <BigText className="headline-3" style={{ color: "#0B70FE", fontWeight: 600 }}>
+                하루 5분 투자로
+                <BigText className="subtitle-2" style={{ color: "black" }}>
+                  미리 준비하는 포트폴리오
+                </BigText>
               </BigText>
-            </BigText>
-            <SmallText className="subtitle-1">중요 정보와 자료를 미리미리 기록!</SmallText>
-          </BgTitleWrapper>
-          <PageImage src="/img/home/img_LT.svg" />
+              <SmallText className="subtitle-1">중요 정보와 자료를 미리미리 기록!</SmallText>
+            </BgTitleWrapper>
+            <PageImage src="/img/home/img_LT.svg" style={{ width: "43.5vw" }} />
+          </div>
           <SliderArrow />
         </BgItemWrapper>
         <BgItemWrapper>
-          <BgTitleWrapper>
-            <EmogiImage src="/img/home/emoji/bulb.svg" />
-            <BigText className="subtitle-2" style={{ color: "#03C348" }}>
-              업무 상황에 맞는
-              <BigText className="subtitle-2" style={{ color: "black" }}>
-                다양한 템플릿 이용 가능
+          <div
+            style={{
+              display: "flex",
+
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "7%",
+            }}
+          >
+            <BgTitleWrapper maxWidth="26%">
+              <EmogiImage src="/img/home/emoji/bulb.svg" />
+              <BigText className="subtitle-2" style={{ color: "#03C348" }}>
+                업무 상황에 맞는
+                <BigText className="subtitle-2" style={{ color: "black" }}>
+                  다양한 템플릿 이용 가능
+                </BigText>
               </BigText>
-            </BigText>
-            <SmallText className="subtitle-1">설계서형, IA형 등 다양한 템플릿</SmallText>
-          </BgTitleWrapper>
-          <PageImage src="/img/home/img_WR.svg" />
+              <SmallText className="subtitle-1">설계서형, IA형 등 다양한 템플릿</SmallText>
+            </BgTitleWrapper>
+            <PageImage src="/img/home/img_WR.svg" style={{ width: "43.5vw" }} />
+          </div>
           <SliderArrow />
         </BgItemWrapper>
         <BgItemWrapper>
-          <BgTitleWrapper>
-            <EmogiImage src="/img/home/emoji/worker.svg" />
-            <BigText className="subtitle-2">
-              디자이너, 개발자 등
-              <BigText className="subtitle-2" style={{ color: "#FF9F00" }}>
-                다양한 협업 히스토리{" "}
-                <span className="subtitle-2" style={{ fontSize: "48px", color: "black" }}>
-                  기록
-                </span>
+          <div
+            style={{
+              display: "flex",
+
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "4.6%",
+            }}
+          >
+            <BgTitleWrapper maxWidth="28.6%">
+              <EmogiImage src="/img/home/emoji/worker.svg" />
+              <BigText className="subtitle-2">
+                디자이너, 개발자 등
+                <BigText className="subtitle-2" style={{ color: "#FF9F00" }}>
+                  다양한 협업 히스토리{" "}
+                  <span className="subtitle-2" style={{ fontSize: "48px", color: "black" }}>
+                    기록
+                  </span>
+                </BigText>
               </BigText>
-            </BigText>
-            <SmallText className="subtitle-1">왓헤픈과 함께 커뮤니케이션 스킬도 UP!</SmallText>
-          </BgTitleWrapper>
-          <PageImage src="/img/home/img_WR.2.svg" />
+              <SmallText className="subtitle-1">왓헤픈과 함께 커뮤니케이션 스킬도 UP!</SmallText>
+            </BgTitleWrapper>
+            <PageImage src="/img/home/img_WR.2.svg" style={{ width: "43.5vw" }} />
+          </div>
           <SliderArrow />
         </BgItemWrapper>
-        <img src="/img/home/03_bottom.svg" style={{ width: "100vw" }} />
+        <img src="/img/home/03_bottom.svg" style={{ width: "100%", flex: 1 }} />
         <Footer />
       </Section>
     </WideWrapper>
