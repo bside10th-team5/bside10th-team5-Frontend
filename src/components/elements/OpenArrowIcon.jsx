f9ccafb308fc95e15f9f9e867eab61e74bbfbd00
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-08-10 08:02:48 +0900

    fix: worktoday 컴포넌트 태그 추가형태변경

--- src/components/elements/OpenArrowIcon.jsx
+++ src/components/elements/OpenArrowIcon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 // import { GRAY500, GRAY900 } from "../../styles/theme";
 
-const OpenArrowIcon = ({ rotate = 0, color = "#111111" }) => {
+const OpenArrowIcon = ({ rotate = 0, color = "#111111", onClick }) => {
   return (
     <svg
       width="28"
@@ -11,6 +11,7 @@ const OpenArrowIcon = ({ rotate = 0, color = "#111111" }) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       style={{ transform: `rotate(${rotate}deg)` }}
+      onClick={onClick}
     >
       <path d="M8.34375 11.877H14.0006H19.6575L14.0006 17.5338L8.34375 11.877Z" fill={color} />
     </svg>
@@ -22,4 +23,5 @@ export default OpenArrowIcon;
 OpenArrowIcon.propTypes = {
   color: PropTypes.string,
   rotate: PropTypes.number,
+  onClick: PropTypes.func,
 };
