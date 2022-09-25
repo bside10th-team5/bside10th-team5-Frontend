1fddec171b21aa3d2819e37d0ff89ec1b9c86e17
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-09-25 21:41:22 +0900

    feat: load template list

--- src/state/writeDiaryState.js
+++ src/state/writeDiaryState.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { format } from "date-fns";
 import { atom, useRecoilValue } from "recoil";
 import { getCookie } from "../utills/cookie";
-import { createRetrospectiveUrl } from "../utills/url";
+import { createRetrospectiveUrl, getRetrospectiveUrl } from "../utills/url";
 
 export const selectedDateState = atom({
   key: "writeDiary/selectedDate",
@@ -20,12 +20,17 @@ export const toggleAddTemplateState = atom({
 });
 
 export const useHandleTemplate = () => {
+  const token = getCookie("token");
+  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+
   const templateList = useRecoilValue(templateListState);
   const date = useRecoilValue(selectedDateState);
-  const saveRetrospective = async (id) => {
-    const token = getCookie("token");
-    axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 
+  const getRetrospective = async (id) => {
+    await axios.get(getRetrospectiveUrl(id, format(date, "yyyy-MM-dd")));
+  };
+
+  const saveRetrospective = async (id) => {
     const convertedTemplateList = templateList.map((el) => {
       if (el.type === "free") {
         return {
@@ -36,9 +41,9 @@ export const useHandleTemplate = () => {
       }
       return el;
     });
-    console.log(convertedTemplateList, "저장전");
+
     return await axios.post(createRetrospectiveUrl(id), convertedTemplateList);
   };
 
-  return { saveRetrospective };
+  return { saveRetrospective, getRetrospective };
 };
--- src/utills/url.js
+++ src/utills/url.js
@@ -2,3 +2,4 @@ export const baseUrl = "https://what-happen.com/api";
 
 export const createBoardUrl = `${baseUrl}/board`;
 export const createRetrospectiveUrl = (id) => `${baseUrl}/board/${id}/retrospective`;
+export const getRetrospectiveUrl = (id, date) => `${baseUrl}/board/${id}/retrospective/${date}`;
